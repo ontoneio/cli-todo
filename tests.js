1919918ab3b278c87e9f01b2bec9c8cba505f827
ontoneio <ontoneio.mm@gmail.com>
2017-09-01 13:15:15 -0700

    refactoring back to mocha chai

--- tests.js
+++ tests.js
@@ -1,41 +1,10 @@
-const test = require('tape')
-const fixFactory = require('fixture-factory')
-const faker = require('faker')
-
+const fix = require('./todo-test-fixtures')
+const module = require('module');
 
 const list = require('./commands/list')
 const add = require('./commands/add')
 const complete = require('./commands/complete')
 const remove = require('./commands/delete')
-
-const testModel = {
-    mainCommand: './task',
-    commands: ['list', 'add', 'complete','delete'],
-    fakeTask: () => {
-        const taskStr = 'Do ' + faker.lorem.sentence()
-        return taskStr
-    }   
-}
-
-fixFactory.register('addTest', testModel)
-const addTestFixture = fixFactory.generate('addTest')
-
-
-
-console.log(addTestFixture)
-
-
-
-
-
-
-// test('Tests for valid argument', (testObj) => {
-//     testObj.plan()
-
-
-
-    
-// })
 
 
 
